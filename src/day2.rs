cccd715b06b5fde81573757c0bbacb2d636e2e2c
Szevin <kevingeiger25@gmail.com>
2023-12-01 17:28:12 +0100

    prepare framework for day2

---
+++ src/day2.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+pub fn solve_a(input_file_path: &str) -> u32 {
+    fs::read_to_string(input_file_path).unwrap().lines()
+}
+
+pub fn solve_b(input_file_path: &str) -> u32 {
+    fs::read_to_string(input_file_path).unwrap().lines()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_a() {
+        assert_eq!(solve_a(".\\src\\test_input\\day2a.txt"), 142);
+    }
+
+    #[test]
+    fn test_solve_b() {
+        assert_eq!(solve_b(".\\src\\test_input\\day2b.txt"), 281);
+    }
+}
